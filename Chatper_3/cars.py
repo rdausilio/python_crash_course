8b4b68ccea719407e973cc2552bd3dc7437cd872
Rowan <rdausilio@gmail.com>
2017-12-28 22:42:36 -0500

    Chapter 3
    Chapter 3 files

---
+++ Chatper_3/cars.py
@@ -0,0 +1,12 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+print(len(cars))
--- Chatper_3/try_it_yourself_8-10.py
+++ Chatper_3/try_it_yourself_8-10.py
@@ -2,3 +2,4 @@
 location_list = ['rome', 'seville', 'bologna', 'berlin']
 print(sorted(location_list))
 print(location_list)
+ #I think I can figure out the rest
