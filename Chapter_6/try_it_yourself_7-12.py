64e996e7968b8c7455b9e2e715822d1e659e058b
Rowan <rdausilio@gmail.com>
2018-01-05 19:21:38 -0500

    Chapter_6
    chapter 6 files

--- Chapter_6/try_it_yourself_7-12.py
+++ Chapter_6/try_it_yourself_7-12.py
@@ -1,1 +1,23 @@
 #6-7 People
+person_1 = {
+    "first_name": "rowan",
+    "last_name": "d'ausilio",
+    "age": 18,
+    "city": "rochester"
+}
+
+person_2 = {
+    "first_name": "kate",
+    "last_name": "dapra",
+    "age": 19,
+    "city": "boston"
+}
+
+person_3 = {
+    "first_name": "lilli",
+    "last_name": "rohde",
+    "age": 19,
+    "city": "poughkeepsie"
+}
+
+people_list = [person_1, person_2, person_3]
